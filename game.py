9e31998e2688a687d71837b07c15d258de13e2cb
ExoPexodus <rushilrana@gmail.com>
2024-05-13 01:05:32 +0530

    added more resonses

--- game.py
+++ game.py
@@ -1,10 +1,35 @@
 import random
 import time
 
+invalid_choice_responses = [
+    "Oh come on! You can do better than this... Fix your spellings!",
+    "Seriously? You're better than that! Try again.",
+    "Invalid choice! Let's try this again.",
+    "Hmm... Are you sure you know how to spell? Try once more."
+]
+
+tie_responses = [
+    "Really!? Wow, you guys ended up in a tie... Congrats, I guess?",
+    "It's a tie! The battle continues...",
+    "A tie! Looks like neither of you wants to give up."
+]
+
+win_responses = [
+    "Good job, you won... for once.",
+    "You're on fire! You beat the computer!",
+    "Congratulations! You've emerged victorious this time."
+]
+
+lose_responses = [
+    "Damn, even the computer can beat you? That's crazy...",
+    "Better luck next time! The computer outsmarted you.",
+    "You'll get 'em next time! Don't give up!"
+]
+
 def get_user_choice():
     user_choice = input("Rock, paper, or scissors? ").lower()
     while user_choice not in ['rock', 'paper', 'scissors']:
-        print("oh come on! you can do better than this.. fix your spellings!")
+        print(random.choice(invalid_choice_responses))
         user_choice = input("Rock, paper, or scissors? ").lower()
     return user_choice
 
@@ -13,13 +38,13 @@ def get_computer_choice():
 
 def determine_winner(user_choice, computer_choice):
     if user_choice == computer_choice:
-        return "really!? wow, you guys ended up in a tie... congrats ig?"
+        return random.choice(tie_responses)
     elif (user_choice == 'rock' and computer_choice == 'scissors') or \
          (user_choice == 'paper' and computer_choice == 'rock') or \
          (user_choice == 'scissors' and computer_choice == 'paper'):
-        return "good job, you won... for once."
+        return random.choice(win_responses)
     else:
-        return "Damn, even the computer can beat you? that's crazy..."
+        return random.choice(lose_responses)
 
 def animate_hand(choice):
     if choice == 'rock':
